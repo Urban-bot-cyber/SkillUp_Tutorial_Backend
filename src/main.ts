6d365920a250add27cdc33b7ccb1fa6b6517b24f
Urban-bot-cyber <prosenjak.urban@gmail.com>
2024-07-21 16:06:39 +0200

    swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import cookieParser from 'cookie-parser'
 import express from 'express'
 import Logging from 'library/Logging'
@@ -18,7 +19,17 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe())
   app.use(cookieParser())
   //Setup to diplay files
-  app.use('/files', express)
+  app.use('/files', express.static('files'))
+
+  //Setup swagger
+  const config = new DocumentBuilder()
+  .setTitle('NestJS tutorial API')
+  .setDescription('This is API for NestJS tutorial')
+  .setVersion('1.0.0')
+  .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('/', app, document)
 
   const PORT = process.env.PORT || 8080
   await app.listen(PORT)
--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -24,56 +24,57 @@ import { join } from 'path'
 import { CreateUserDto } from './dto/create-user.dto'
 import { UpdateUserDto } from './dto/update-user.dto'
 import { UsersService } from './users.service'
+import { ApiBadRequestResponse, ApiCreatedResponse, ApiTags } from '@nestjs/swagger'
 
+@ApiTags('users')
 @Controller('users')
 @UseInterceptors(ClassSerializerInterceptor)
 export class UsersController {
-  constructor(private readonly userService: UsersService) {}
+    constructor(private readonly userService: UsersService) {
 
-  @HasPermission('users')
-  @Get()
-  @HttpCode(HttpStatus.OK)
-  async findAll(@Query('page') page: number): Promise<PaginatedResult> {
-    return this.userService.paginate(page, ['role'])
-  }
-
-  @Get(':id')
-  @HttpCode(HttpStatus.OK)
-  async findOne(@Param('id') id: string): Promise<User> {
-    return this.userService.findById(id)
-  }
-
-  @Post()
-  @HttpCode(HttpStatus.CREATED)
-  async create(@Body() createUserDto: CreateUserDto): Promise<User> {
-    return this.userService.create(createUserDto)
-  }
-
-  @Post('upload/:id')
-  @UseInterceptors(FileInterceptor('avatar', saveImageToStorage))
-  @HttpCode(HttpStatus.CREATED)
-  async upload(@UploadedFile() file: Express.Multer.File, @Param('id') id: string): Promise<User> {
-    const filename = file?.filename
-    if (!filename) throw new BadRequestException('File must be png, jpg/jpeg.')
-
-    const imagesFolderPath = join(process.cwd(), 'files')
-    const fullImagePath = join(imagesFolderPath + '/' + file.filename)
-    if (await isFileExtensionSafe(fullImagePath)) {
-      return this.userService.updateUserImageId(id, filename)
     }
-    removeFile(fullImagePath)
-    throw new BadRequestException('File content does not mathc extesion.')
-  }
+    @ApiCreatedResponse({ description: 'List of users.' })
+    @ApiBadRequestResponse({ description: 'Error for list of users.' })
+    @Get()
+    @HttpCode(HttpStatus.OK)
+    async findAll(@Query('page') page: number): Promise<PaginatedResult> {
+        return this.userService.paginate(page, ['role'])
+    }
+    @Get(':id')
+    @HttpCode(HttpStatus.OK)
+    async findOne(@Param('id') id: string): Promise<User> {
+        return this.userService.findById(id)
+    }
 
-  @Patch(':id')
-  @HttpCode(HttpStatus.OK)
-  async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto): Promise<User> {
-    return this.userService.update(id, updateUserDto)
-  }
-
-  @Delete()
-  @HttpCode(HttpStatus.OK)
-  async delete(@Param('id') id: string): Promise<User> {
-    return this.userService.remove(id)
-  }
+    @ApiCreatedResponse({ description: 'Creates new user.' })
+    @ApiBadRequestResponse({ description: 'Error for creting a new user.' })
+    @Post()
+    @HttpCode(HttpStatus.CREATED)
+    async create(@Body() createUserDto: CreateUserDto): Promise<User> {
+        return this.userService.create(createUserDto)
+    }
+    @Post('upload/:id')
+    @UseInterceptors(FileInterceptor('avatar', saveImageToStorage))
+    @HttpCode(HttpStatus.CREATED)
+    async upload(@UploadedFile() file: Express.Multer.File, @Param('id') id: string): Promise<User> {
+        const filename = file?.filename
+        if (!filename) throw new BadRequestException('File must be png, jpg/jpeg.')
+        const imagesFolderPath = join(process.cwd(), 'files')
+        const fullImagePath = join(imagesFolderPath + '/' + file.filename)
+        if (await isFileExtensionSafe(fullImagePath)) {
+            return this.userService.updateUserImageId(id, filename)
+        }
+        removeFile(fullImagePath)
+        throw new BadRequestException('File content does not mathc extesion.')
+    }
+    @Patch(':id')
+    @HttpCode(HttpStatus.OK)
+    async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto): Promise<User> {
+        return this.userService.update(id, updateUserDto)
+    }
+    @Delete()
+    @HttpCode(HttpStatus.OK)
+    async delete(@Param('id') id: string): Promise<User> {
+        return this.userService.remove(id)
+    }
 }
